1ed80c26af09a3f437eb918e80d7784814288d17
NamVH <namvh@coin98.finance>
2024-05-04 11:57:55 +0700

    feat: split page

---
+++ src/Components/Course/index.js
@@ -0,0 +1,7 @@
+import { Button } from "@nextui-org/button";
+
+function Course() {
+  return <Button color="primary">Button</Button>;
+}
+
+export default Course;
---
+++ src/Components/Profile/index.js
@@ -0,0 +1,7 @@
+import { Button } from "@nextui-org/button";
+
+function Profile() {
+  return <Button color="primary">Button</Button>;
+}
+
+export default Profile;
